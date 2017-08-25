4cdcec65427928de592d2391ce9a27a3525ac6ab
jianghouwei <jianghouwei@xs.com>
2017-08-25 11:55:04 +0800

    1232132

--- fox-demo/fox-demo-server/src/main/java/com/sun/fox/demo/server/foxdemoserver/demo/service/DemoService.java
+++
@@ -1,8 +0,0 @@
-package com.sun.fox.demo.server.foxdemoserver.demo.service;
-
-import com.sun.fox.demo.api.foxdemoapi.pojo.DemoDto;
-
-public interface DemoService {
-
-    public DemoDto getDemoObj();
-}
