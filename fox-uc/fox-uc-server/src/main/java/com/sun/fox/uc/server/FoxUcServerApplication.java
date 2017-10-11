1bf8c043e8424fc49923903bf2e34751487c8a7e
jianghouwei <jianghouwei@xs.com>
2017-10-11 10:37:33 +0800

    用户中心

---
+++ fox-uc/fox-uc-server/src/main/java/com/sun/fox/uc/server/FoxUcServerApplication.java
@@ -0,0 +1,12 @@
+package com.sun.fox.uc.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoxUcServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoxUcServerApplication.class, args);
+	}
+}
