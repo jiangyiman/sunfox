a6216ae0f8a1053e6f9ef26d28e2cf8114a640ae
jianghouwei <jianghouwei@xs.com>
2017-11-07 13:27:21 +0800

    用户中心

--- fox-member/fox-member-server/src/main/java/com/sun/fox/member/server/member/service/MemberInfoService.java
+++
@@ -1,8 +0,0 @@
-package com.sun.fox.member.server.member.service;
-
-import com.sun.fox.member.server.member.model.MemberInfo;
-import com.sun.fox.mybatis.base.server.BaseService;
-
-
-public interface MemberInfoService extends BaseService<MemberInfo> {
-}
