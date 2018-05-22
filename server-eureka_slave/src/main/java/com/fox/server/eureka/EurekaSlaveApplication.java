a90a4e24b200c01478ad49ce16c499877e1ee542
jianghouwei <jianghouwei@xs.com>
2018-05-22 08:52:22 +0800

    更新配置

--- server-eureka_slave/src/main/java/com/fox/server/eureka/EurekaSlaveApplication.java
+++ server-eureka_slave/src/main/java/com/fox/server/eureka/EurekaSlaveApplication.java
@@ -2,13 +2,15 @@ package com.fox.server.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableEurekaClient
 public class EurekaSlaveApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaSlaveApplication.class, args);
-	}
+    public static void main( String[] args ) {
+        SpringApplication.run(EurekaSlaveApplication.class, args);
+    }
 }
